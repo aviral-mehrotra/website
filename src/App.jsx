4e1fbb30c25369598d806c4bff48b2fcbf0b27f4
Aviral Mehrotra <aviral.mehrotra@gmail.com>
2024-03-03 20:33:29 -0600

    playing with backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Spline from '@splinetool/react-spline';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="background">
+        <Spline scene="https://prod.spline.design/99Bl7ZsB1K7DCOre/scene.splinecode"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Aviral Mehrotra</h1>
+      <p>
+        I'm a first-year student at the University of Chicago
+      </p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
