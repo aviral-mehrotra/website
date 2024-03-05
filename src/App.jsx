5b4e3dc3481d9a8bb76d4e67f79e5483d85066d0
Aviral Mehrotra <aviral.mehrotra@gmail.com>
2024-03-05 16:14:28 -0600

    modified website

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,59 @@
 import Spline from '@splinetool/react-spline';
+import { SocialIcon } from 'react-social-icons/component'
+import 'react-social-icons/github'
+import 'react-social-icons/linkedin'
+import 'react-social-icons/medium'
+import 'react-social-icons/instagram'
 import './App.css'
+
 
 function App() {
 
+  const centerDivStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh'
+  }
+
+  const socialIconStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
+  const headerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
   return (
     <>
-      <div className="background">
-        <Spline scene="https://prod.spline.design/99Bl7ZsB1K7DCOre/scene.splinecode"/>
+      <div style={ centerDivStyle }>
+        <div style={ headerStyle }>
+          <h1>Aviral Mehrotra</h1>
+          <p style={{ fontSize: '1.2em', whiteSpace: 'pre-wrap', width: 500, paddingBottom: '2.4em' }}>
+            I'm a first-year student at the University of Chicago studying Computer Science and Business Economics
+          </p>
+          <div style={ socialIconStyle }>
+            <div style={{ marginRight: '3px' }}>
+              <SocialIcon bgColor="#888" network="linkedin" url="https://www.linkedin.com/in/aviralmehrotra" target="_blank"/>
+            </div>
+            <div style={{ marginLeft: '3px', marginRight: '3px' }}>
+              <SocialIcon bgColor="#888" network="github" url="https://github.com/aviral-mehrotra" target="_blank"/>
+            </div>
+            <div style={{ marginLeft: '3px', marginRight: '3px' }}>
+              <SocialIcon bgColor="#888" network="medium" url="https://medium.com/@aviral.mehrotra" target="_blank"/>
+            </div>
+            <div style={{ marginLeft: '3px' }}>
+              <SocialIcon bgColor="#888" network="instagram" url="https://www.instagram.com/aviral_mehrotra/" target="_blank"/>
+            </div>
+            
+          </div>
+        </div>
       </div>
-      <h1>Aviral Mehrotra</h1>
-      <p>
-        I'm a first-year student at the University of Chicago
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
